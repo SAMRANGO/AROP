5c18e189b8941655f6293111fbae90d2d7715c23
astrod333 <130163473+astrod333@users.noreply.github.com>
2025-07-01 16:43:53 +0300

    refactor(auth): move subscription check to middleware
    Move subscription status check from dashboard page to middleware for better security and consistency
    Update tailwind-merge dependency to latest version

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,29 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { fetchQuery } from 'convex/nextjs';
 import { NextResponse } from 'next/server';
+import { api } from './convex/_generated/api';
 
 const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
+
+  const token = (await (await auth()).getToken({ template: "convex" }))
+
+
+  const { hasActiveSubscription } = await fetchQuery(api.subscriptions.getUserSubscriptionStatus, {
+  }, {
+    token: token!,
+  });
+
   const isDashboard = req.nextUrl.href.includes(`/dashboard`)
 
-  // You can only check authentication here, not subscription status
-  if (isProtectedRoute(req)) await auth.protect()
+  if (isDashboard && !hasActiveSubscription) {
+    const pricingUrl = new URL('/pricing', req.nextUrl.origin)
+    // Redirect to the pricing page
+    return NextResponse.redirect(pricingUrl);
+  }
 
-  // If you want to redirect users without a subscription, move that logic to a server-side API route or in the dashboard page itself
+  if (isProtectedRoute(req)) await auth.protect()
 })
 
 export const config = {
